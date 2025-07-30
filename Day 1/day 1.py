dba3e7455a9d7eeeb8c460c468a522656f8e573e
Renato <fortnitekidbr56@gmail.com>
2025-07-30 00:42:34 -0400

    Initial commit

---
+++ Day 1/day 1.py
@@ -0,0 +1,21 @@
+# 1:
+
+print("Hello, world!")
+
+print(2 + 3) 
+
+print("2 + 3 =", 2 + 3)
+
+# 2:
+name = "Renato"  # string
+
+years_old = 16   # int
+
+height = 1.80    # float
+
+studies = True   # boolean
+
+print(name)
+print(years_old)
+print("Height:", height)
+print("Does he study?", studies)
---
+++ Day 1/ex.py
@@ -0,0 +1,20 @@
+name = "Fred"
+yo = 15
+city = "New York"
+
+print("Name", name)
+print("Years old", yo)
+print("Lives in", city)
+
+
+grade1 = 7
+grade2 = 8
+grade3 = 5
+
+average = (7 + 8 + 5) / 3
+
+print(f"Average {average:.2f}.")
+
+breakfast = True
+
+print("Is there breakfast?", breakfast)
